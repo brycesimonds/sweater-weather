a057f9fba43c62c8556d9629c89d8c3c7e38bcd2
Bryce Simonds <bsimonds14@gmail.com>
2022-09-26 13:51:55 -0600

    Test: post call creates a user and sends back a 201 response with json and a generated api key

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'User data' do
+  it "create: api call can create user in db and send 201 response with JSON and api_key", :vcr do 
+
+    post "/api/v1/users" 
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    user_data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(user_data).to include(:type, :id, :attributes)
+    expect(user_data[:type]).to eq("users")
+    expect(user_data[:id]).to be_a String
+    expect(user_data[:attributes]).to be_a Hash
+    expect(user_data[:attributes]).to include(:email, :api_key)
+    expect(user_data[:attributes][:email]).to be_a String
+    expect(user_data[:attributes][:api_key]).to be_a String
+  end
+end
