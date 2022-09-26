0358e621f1dcc4087c335ec594de624c6c281eec
Bryce Simonds <bsimonds14@gmail.com>
2022-09-26 14:57:30 -0600

    Happy path feat: user is created when given valid data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,10 @@
+require 'securerandom'
 class Api::V1::UsersController < ApplicationController
   def create
-    if params[:user][:password] == params[:user][:password_confirmation]
-      user = User.new(email: params[:user][:email], password: params[:user][:password], api_key: random_api_key)
-      if user.save
-        render json: UserSerializer.create_user(email: params[:user][:email], password: params[:user][:password], api_key: random_api_key), status: 201
-      else
-        flash[:alert] = "Error: #{error_message(user.errors)}"
-        render status: 400
-      end
-    else
-      flash[:alert] = "Error: #{error_message(user.errors)}"
+    if params[:user][:password] == params[:password_confirmation]
+      json_response(UserSerializer.create_user(User.create(email: params[:user][:email], password: params[:user][:password], api_key: SecureRandom.hex)), :created)
+    else 
+      puts "idk"
     end 
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'User data' do
   it "create: api call can create user in db and send 201 response with JSON and api_key", :vcr do 
-    user_params = {
+    user_params = ({
       email: "whatever@example.com",
       password: "password",
       password_confirmation: "password"
-    }
+    })
     headers = {"CONTENT_TYPE" => "application/json"}
 
     post "/api/v1/users" , headers: headers, params: JSON.generate(user_params)
-
 
     expect(response).to be_successful
     expect(response.status).to eq(201)
@@ -21,7 +20,5 @@ RSpec.describe 'User data' do
     expect(created_user.email).to eq('whatever@example.com')
     expect(created_user.password).to eq(user_params[:password])
     expect(created_user.password).to eq('password')
-    expect(created_user.password_confirmation).to eq(user_params[:password_confirmation])
-    expect(created_user.password_confirmation).to eq('password')
   end
 end
