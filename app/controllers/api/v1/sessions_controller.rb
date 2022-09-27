62e4c5486ce930c90b2c45a08070f4bf03ceae35
Bryce Simonds <bsimonds14@gmail.com>
2022-09-27 15:28:42 -0600

    Debug: fixed sessions create to authenticate password check

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user.present? && user.password == params[:password]
+    if user&.authenticate(params[:password])
       render json: UserSerializer.send_user_email_and_api_key(user), status: 200
     else 
       render json: { errors: 'Passwords do not match, fields are missing, or there is no account associated with the given email' }, status: 400
