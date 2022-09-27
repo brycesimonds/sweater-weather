7e141a250b24a836c7c1015adb42ad9a1a90f3a4
Bryce Simonds <bsimonds14@gmail.com>
2022-09-27 11:44:24 -0600

    Test: mapquest service can get directions

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -22,4 +22,18 @@ RSpec.describe MapquestService do
     expect(single_result[:locations][0][:latLng][:lat]).to be_a Float 
     expect(single_result[:locations][0][:latLng][:lng]).to be_a Float 
   end
+
+  it 'retrieves directions from point a to point b', :vcr do 
+    start_city = "Denver,CO"
+    end_city = "Boston,MA"
+
+    parsed_json = MapquestService.get_directions(start_city, end_city)
+
+    expect(parsed_json).to be_a Hash
+
+    result = parsed_json[:route]
+
+    expect(result).to include(:realTime)
+    expect(result[:realTime]).to be_a Integer
+  end
 end
